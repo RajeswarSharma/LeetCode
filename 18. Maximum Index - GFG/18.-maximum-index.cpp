6b739572f3097dffa11ce18d53e4cd3a36c65180
Rajeswar Sharma <54684919+RajeswarSharma@users.noreply.github.com>
2022-04-20 02:00:07 +0530

    Added solution - LeetHub

---
+++ 18. Maximum Index - GFG/18.-maximum-index.cpp
@@ -0,0 +1,74 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+        
+    // A[]: input array
+    // N: size of array
+    // Function to find the maximum index difference.
+    int maxIndexDiff(int arr[], int N) 
+    { 
+       
+        int max_dis = -1;
+        int curr_max = N-1;
+        vector<int>aux(N);
+        for(int i =N-1;i>=0;i--){
+            if(arr[curr_max] < arr[i]){
+                curr_max = i;
+            }
+            aux[i] = curr_max;
+        }
+        
+        int x=0, y = aux[0];
+        
+        max_dis = -1;
+        // for(int i =0;i<N;i++) cout<<aux[i]<<" ";
+        // cout<<endl;
+        while(y<N&&x<=y){
+            
+            if(arr[x]<=arr[y])
+            {
+                max_dis = max(max_dis,y-x);
+                if(y+1>=N) break;
+                y = aux[y+1];
+                // cout<<y<<endl;
+            }
+            
+            else x++;
+            
+        }
+          
+          return max_dis;
+       
+    }
+};
+
+// { Driver Code Starts.
+  
+/* Driver program to test above functions */
+int main() 
+{
+    int T;
+    //testcases
+    cin>>T;
+    while(T--){
+        int num;
+        //size of array
+        cin>>num;
+        int arr[num];
+        
+        //inserting elements
+        for (int i = 0; i<num; i++)
+            cin>>arr[i];
+        Solution ob;
+        
+        //calling maxIndexDiff() function
+        cout<<ob.maxIndexDiff(arr, num)<<endl;    
+        
+    }
+    return 0;
+}   // } Driver Code Ends
